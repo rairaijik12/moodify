b85e34cbebfb079299c4dd9713b3fe48676d67bc
OsirisXxx <justtacee@gmail.com>
2025-04-18 09:24:55 +0800

    Yo5

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,12 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
+const path = require('path');
 
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname);
+
+// Add custom path resolution
+config.resolver.alias = {
+  '@': path.resolve(__dirname, './')
+};
 
 module.exports = withNativeWind(config, { input: "./app/globals.css" });
